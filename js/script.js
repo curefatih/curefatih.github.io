1db5a0e8fadfdf8678d0bc3edf264cd397357b80
Fatih Cüre <curefat@gmail.com>
2021-03-01 00:06:47 +0300

    deleted old folders

--- js/script.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    feather.replace()
-    document.addEventListener("DOMContentLoaded", () => {
-        setTimeout(() => {
-            document.querySelector("#loader").classList.add("hide");
-            document.body.classList.remove("locked");
-        }, 500)
-    }, false);
-})()
